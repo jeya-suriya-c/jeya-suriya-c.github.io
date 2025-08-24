fb00446a835695f76b6dad741eaabc69a1aa10c7
Jeya Suriya <cjsuriya96@gmail.com>
2025-08-25 03:16:32 +0530

    Ok

--- script.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById('contactForm');
-const msg = document.getElementById('msg');
-
-form.addEventListener('submit', function(e){
-  e.preventDefault();
-  msg.textContent = "Thank you! Your message has been sent.";
-  form.reset();
-});
